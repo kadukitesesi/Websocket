c54aea3b8ad66761971c994b25cc6f2c7dd5857b
Carlos <kadukitesesi@gmail.com>
2024-08-27 13:42:37 -0300

    Projeto websocket

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,5 +1,5 @@
 const stompClient = new StompJs.Client({
-    brokerURL: 'ws://localhost:8080/websocket'
+    brokerURL: 'https://websocket-production-711e.up.railway.app'
 });
 
 stompClient.onConnect = (frame) => {
