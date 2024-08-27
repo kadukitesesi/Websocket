c730786e88e112916189549668f59b3d348b7105
Carlos <kadukitesesi@gmail.com>
2024-08-27 13:51:29 -0300

    Projeto websocket

--- src/main/java/com/chat/realtime/config/WebSocketConfig.java
+++ src/main/java/com/chat/realtime/config/WebSocketConfig.java
@@ -5,7 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -17,10 +17,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/app");
     }
 
+    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket");
-
+        registry.addEndpoint("/websocket")
+                .setAllowedOrigins("https://websocket-production-711e.up.railway.app/")
+                .withSockJS();
     }
 
-
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://websocket-production-711e.up.railway.app/")
+                .allowedMethods("*");
+    }
 }
